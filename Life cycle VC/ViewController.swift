e293ac207f4b06c68e1b087a4f9cdb3136d381fe
George <georgii.kashin@gmail.com>
2019-04-28 15:41:18 +0300

    Initial Commit

---
+++ Life cycle VC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Life cycle VC
+//
+//  Created by Георгий Кашин on 28/04/2019.
+//  Copyright © 2019 Georgii Kashin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
